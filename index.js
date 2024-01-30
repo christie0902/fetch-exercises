7d9e90b0ff38cbb58cbe14b10e8f79f0ff9fc006
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-01-30 20:38:20 +0100

    completed basic logic

--- index.js
+++ index.js
@@ -28,3 +28,85 @@ const createFact = (fact) => {
 factBtn.addEventListener("click", (event) => {
   loadData();
 });
+
+{
+  /* <table>
+        <tr>
+          <th>Token</th>
+          <th>Price Change</th>
+          <th>Price Change in Percentage</th>
+       
+        </tr>
+         <tr>
+          <td></td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+       </tr>
+ </table> */
+}
+const table = document.querySelector("table");
+let row;
+const getData = async () => {
+  const response = await fetch("https://api2.binance.com/api/v3/ticker/24hr");
+  const data = await response.json();
+  const ethBtc = data[1];
+  const ltcBtc = data[2];
+  const bnbBtc = data[3];
+  deleteData()
+  displayData(ethBtc);
+  displayData(ltcBtc);
+  displayData(bnbBtc);
+};
+
+
+const displayData = (data) => {
+  row = table.insertRow();
+  row.classList.add("row");
+
+  const symbol = row.insertCell(0);
+  const priceChange = row.insertCell(1);
+  const weightedAvgPrice = row.insertCell(2);
+  const lastPrice = row.insertCell(3);
+  const lastQty = row.insertCell(4);
+  const bidPrice = row.insertCell(5);
+  const bidQty = row.insertCell(6);
+  const askPrice = row.insertCell(7);
+  const askQty = row.insertCell(8);
+  const highPrice = row.insertCell(9);
+  const lowPrice = row.insertCell(10);
+
+  symbol.textContent = `${data.symbol}`;
+  priceChange.textContent = `${data.priceChange}`;
+  weightedAvgPrice.textContent = `${data.weightedAvgPrice}`;
+  lastPrice.textContent = `${data.lastPrice}`;
+  lastQty.textContent = `${data.lastQty}`;
+  bidPrice.textContent = `${data.bidPrice}`;
+  bidQty.textContent = `${data.bidQty}`;
+  askPrice.textContent = `${data.askPrice}`;
+  askQty.textContent = `${data.askQty}`;
+  highPrice.textContent = `${data.highPrice}`;
+  lowPrice.textContent = `${data.lowPrice}`;
+
+  highPrice.style.backgroundColor = "green";
+  lowPrice.style.backgroundColor = "red";
+};
+
+getData();
+
+setInterval(()=> {
+  getData();
+} , 3000)
+
+const deleteData = () => {
+  for (let i = 0; i < 3; i++) {
+        const rowCount = table.rows.length;
+        if (rowCount > 1) {
+          table.deleteRow(rowCount - 1);
+        }
+      }
+}
